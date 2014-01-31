fe7455211914f5137cd40676bbc7e185bcef7647
Nicolas Poirier-Barolet <n.poirier@la-grange.ca>
2014-01-31 14:56:32 -0500

    Ajouté .gitignore, gruntfile.js, package.json et structure de dossier.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,4 +3,7 @@ CHANGELOG
 
 Version 0.0.1
 -----------------
+
+### 31-01-2013
+* Ajout de .gitignore, gruntfile.js, package.json et structure de dossier.
 * Création du projet
---
+++ boilerplate/Gruntfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    // pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;'+
+      '*/',
+    // Task configuration.
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['js/<%= pkg.name %>.js'],
+        dest: 'js/<%= pkg.name %>.concat.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'js/<%= pkg.name %>.min.js'
+      },
+    },
+    watch: {
+      less: {
+        files: 'less/**/*.less',
+        tasks: ['less']
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: [],
+          compress : true
+        },
+        files: {
+          "css/master.css" : "less/master.less"
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task.
+  grunt.registerTask('default', ['concat', 'uglify', 'less']);
+
+};
